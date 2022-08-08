89f00ec96691cbd58ff974d7b81c5404a42fbd13
Joe Nash <joseph.nash@render-se.com>
2022-08-08 13:57:36 -0400

    updated package installer to upgrade before install

--- packages-install.sh
+++ packages-install.sh
@@ -3,5 +3,5 @@
 # Install packages for fresh Fedora 36 Silverblue
 
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-sudo rpm-ostree install binutils clang code gnome-tweak-tool i3-gaps neovim network-manager-applet nitrogen openssl polybar qemu-user-static rofi ShellCheck stow vim-common
+sudo rpm-ostree update && sudo rpm-ostree upgrade && sudo rpm-ostree install binutils clang code gnome-tweak-tool i3-gaps neovim network-manager-applet nitrogen openssl polybar qemu-user-static rofi ShellCheck stow vim-common
 
