eed7f1dfa715c50bc7554f686b785f7b29f6b7b8
jrn90 <jrnash20650@gmail.com>
2023-01-04 13:24:14 -0500

    Removed nitrogen, update readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# silverblue
+# Silverblue 37
+
+Contains my Silverblue 37 configuration.
+
+## Install Steps
+
+1. Run `packages-install.sh`
+2. Run `install.sh`
+
--- copy-configs.sh
+++ copy-configs.sh
@@ -8,8 +8,6 @@ cp "$HOME"/.config/i3/config ./i3/
 cp "$HOME"/.config/polybar/config.ini ./polybar/
 cp "$HOME"/.config/polybar/launch.sh ./polybar/
 cp "$HOME"/.Xresources ./x/
-cp "$HOME"/.config/nitrogen/bg-saved.cfg ./nitrogen/
-cp "$HOME"/.config/nitrogen/nitrogen.cfg ./nitrogen/
 
 cp "$HOME"/.walls/wall.png ./walls/
 
