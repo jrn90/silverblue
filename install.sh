c12ff0569356ff10c1ab0fcbc08a24e8e165c36a
jrn90 <jrnash20650@gmail.com>
2023-01-04 12:11:48 -0500

    removed nitrogen

--- install.sh
+++ install.sh
@@ -8,7 +8,6 @@ CONFIG_DIR="$HOME/.config"
 FONT_DIR="$HOME/.fonts"
 I3_DIR="$HOME/.config/i3"
 POLYBAR_DIR="$HOME/.config/polybar"
-NITROGEN_DIR="$HOME/.config/nitrogen"
 WALLS_DIR="$HOME/.walls"
 ROFI_DIR="$HOME/.config/rofi"
 ROFI_THEMES_DIR="$HOME/.local/share/rofi/themes/"
@@ -29,10 +28,6 @@ if [ ! -d "$POLYBAR_DIR" ]; then
   mkdir "$POLYBAR_DIR"
 fi
 
-if [ ! -d "$NITROGEN_DIR" ]; then
-  mkdir "$NITROGEN_DIR"
-fi
-
 if [ ! -d "$WALLS_DIR" ]; then
   mkdir "$WALLS_DIR"
 fi
@@ -48,8 +43,6 @@ fi
 cp ./i3/config "$I3_DIR"
 cp ./polybar/config.ini "$POLYBAR_DIR"
 cp ./polybar/launch.sh "$POLYBAR_DIR"
-cp ./nitrogen/bg-saved.cfg "$NITROGEN_DIR"
-cp ./nitrogen/nitrogen.cfg "$NITROGEN_DIR"
 cp ./walls/wall.png "$WALLS_DIR"
 cp ./rofi/config.rasi "$ROFI_DIR"
 cp ./rofi/catppuccin-mocha.rasi "$ROFI_THEMES_DIR"
